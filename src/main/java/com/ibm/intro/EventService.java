e21a58a2d50047b97219ebb3728940fc1928fa52
Matthias <wal16412@spengergasse.at>
2020-10-28 19:14:18 +0100

    implement eventService

--- src/main/java/com/ibm/intro/EventService.java
+++ src/main/java/com/ibm/intro/EventService.java
@@ -1,20 +1,49 @@
 /**
  * star-challenge
- *
+ * <p>
  * (C) Copyright 2016 IBM Corporation
  * All rights reserved
- *
+ * <p>
  * Creation date: 03.08.2016
  */
 package com.ibm.intro;
 
+import com.ibm.intro.exception.DataStoreException;
+import com.ibm.intro.exception.OptimisticLockingException;
+import com.ibm.intro.model.Event;
+import com.ibm.intro.model.Status;
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
 /**
+ * Service for processing events
+ *
  * @author Richard Holzeis
  */
 public class EventService {
 
-	/*
-	 * #task 3: implement me
-	 * 
-	 */
+    private final ProductionService productionService;
+    private final HashMap<EnumKeyPair, Status> processMap;
+    private final Logger logger;
+    private final ExecutorService executorService;
+
+    public EventService() {
+        this.productionService = new ProductionService();
+        this.processMap = ProcessMap.getInstance();
+        this.logger = Logger.getAnonymousLogger();
+        this.executorService = Executors.newCachedThreadPool();
+    }
+
+
+    /**
+     * Handles the execution of the processing callables
+     * @param event the event to be processed
+     */
+    public void process(Event event) {
+        EventProcessingCallable processingCallable = new EventProcessingCallable(event, productionService, logger, processMap);
+        executorService.submit(processingCallable);
+    }
 }
--- src/test/java/com/ibm/intro/EventServiceTest.java
+++ src/test/java/com/ibm/intro/EventServiceTest.java
@@ -8,12 +8,34 @@
  */
 package com.ibm.intro;
 
+import com.ibm.intro.exception.DataStoreException;
+import com.ibm.intro.model.*;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 /**
  * @author Richard Holzeis
  */
 public class EventServiceTest {
-	
-	/*
-	 * #task 3: implement me! 
-	 */
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void testProcessEvent() throws DataStoreException, InterruptedException {
+        var productionService = new ProductionService();
+        var eventService = new EventService();
+
+        var production = new Production(ProductionType.LINEHAUL, 1L);
+        production = productionService.create(production);
+
+        var event = new Event(EventType.EXIT, production.getId());
+
+        eventService.process(event);
+        Thread.sleep(20);
+
+        Assert.assertEquals(Status.INPRODUCTION, productionService.read(event.getProductionId()).getStatus());
+    }
 }
